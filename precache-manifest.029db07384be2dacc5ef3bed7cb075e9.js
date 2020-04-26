de1352da1bc8e2945544b57042115301fdd0117c
msg824 <skfkgkdlaos@gmail.com>
2020-04-26 23:49:54 +0900

    Updates

---
+++ precache-manifest.029db07384be2dacc5ef3bed7cb075e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1571ff66e2569feb0345dfcf93954f3a",
+    "url": "/yourics/index.html"
+  },
+  {
+    "revision": "e2977ffe407381f2f3a1",
+    "url": "/yourics/static/css/main.b0934ba3.chunk.css"
+  },
+  {
+    "revision": "4a47308ee12de190a1fd",
+    "url": "/yourics/static/js/2.1d873a46.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/yourics/static/js/2.1d873a46.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2977ffe407381f2f3a1",
+    "url": "/yourics/static/js/main.c2da8c0c.chunk.js"
+  },
+  {
+    "revision": "57ec5b2958326d4ec438",
+    "url": "/yourics/static/js/runtime-main.add69c28.js"
+  }
+]);
