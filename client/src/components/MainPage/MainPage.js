dacd5b9aa265104fdcce5967e0199dd98157f61d
msg824 <skfkgkdlaos@gmail.com>
2020-04-26 21:57:00 +0900

    React router 로 각 페이지 연결 서버, 클라이언트 동시 실행 가능 명령어 : npm run dev

---
+++ client/src/components/MainPage/MainPage.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './css/MainPage.css';
+
+class MainPage extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            
+        }
+    }
+
+    componentDidMount() {
+
+    }
+
+    render() {
+        return (
+            <div className="container_main">
+                메인 페이지
+            </div>
+        ) 
+    }
+}
+
+export default MainPage;
