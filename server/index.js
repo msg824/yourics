d49c53b3d5449440059bb9c6ec135bdfdaba0b1f
msg824 <skfkgkdlaos@gmail.com>
2020-05-11 01:13:19 +0900

    주석 삭제

--- server/index.js
+++ server/index.js
@@ -4,9 +4,6 @@ const axios = require('axios');
 const { google } = require('googleapis');
 const sequelize = require('./models').sequelize;
 
-// // 환경변수가 production 이 아닐 경우 development
-// process.env.NODE_ENV = ( process.env.NODE_ENV && ( process.env.NODE_ENV ).trim().toLowerCase() == 'production' ) ? 'production' : 'development';
-
 let configs = {};
 process.env.NODE_ENV === 'production' ? configs = require('./config/production') : configs = require('./config/development');
 
@@ -26,40 +23,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use('/youtube', youtubeApi);
 
 const port = process.env.PORT || 5000;
+
+// Sequelize 연결 후 Node server 연결
 sequelize.sync({ force: false }).then(() => {
     app.listen(port, () => console.log(`Listening on port ${port}`));
 })
-
-
-// // API 할당량 테스트
-// const youtube = google.youtube({
-//     version: 'v3',
-//     auth: configs.youtubeApi
-// })
-
-// async function searchList(song) {
-//     try {
-//         const songName = '';
-
-//         if (!songName) {
-//             console.log(songName, 'asdf')
-//             const res = await youtube.search.list({
-//                 part: 'snippet',
-//                 q: '소주한잔',
-//                 maxResults: '1',
-//                 type: 'video',
-//                 // fields: 'items(id(videoId))'
-//             });
-
-//             console.log('res.data.items', res.data.items[0]);
-//             console.log('res.data.items.snippet.thumbnails', res.data.items[0].snippet.thumbnails)
-//         }
-        
-
-//     } catch (error) {
-//         console.log('youtube search API Error', error)
-//     }
-    
-// }
-
-// searchList();
