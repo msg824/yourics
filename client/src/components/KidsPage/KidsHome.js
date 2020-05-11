447a4a9086a9c3ae0849b61149ddf64a5f737438
msg824 <skfkgkdlaos@gmail.com>
2020-05-12 03:16:13 +0900

    Merge remote-tracking branch 'origin/pilwon' into minsung-pc
    # Conflicts:
    #	client/src/components/MainPage/MainPage.js
    #	package.json

--- client/src/components/KidsPage/KidsHome.js
+++ client/src/components/KidsPage/KidsHome.js
@@ -1,5 +1,32 @@
 import React from 'react';
 import './css/KidsHome.css';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+import KidsPinkpong from './KidsPinkpong';
+//import { FacebookButton, FacebookCount } from "react-social-kr";
+
+// // React Router 정의
+// function App() {
+//     return (
+//         <div className="KidsHome.js">
+//             <Router>
+//                 <Switch>
+//                     <Route exact path="/" children={<Home/>} /> {/* 아래 Home 함수 호출 */}
+//                     <Route path="/main" children={<MainPage/>} /> {/* MainPage 호출 */}
+//                     <Route path="/kids" children={<KidsPage/>} /> {/* KidsPage 호출 */}
+//                     <Route path="/pinkpong" children={<KidsPinkpong/>} /> {/* KidsPinkpong Page 호출 */}
+//                 </Switch>
+//             </Router>
+//         </div>
+//     );
+// }
+
+
+
+
+// @connect(
+//     (state, ownProps) => ({
+//       pathname: ownProps.location.pathname
+//     }), {})
 
 class KidsHome extends React.Component {
     constructor(props) {
@@ -14,9 +41,59 @@ class KidsHome extends React.Component {
     }
 
     render() {
+
         return (
-            <div className="container_kids">
-                키즈 페이지
+            
+            <div className="container-kids">
+
+            <div className="KidsPinkpong">
+                <Router>
+                    <Switch>
+                        <Route path="/pinkpong" component={KidsPinkpong} /> {/* KidsPinkpong Page 호출 */}
+                    </Switch>
+                </Router>
+            </div>
+
+                <div className="backhome2">
+                    <a href="http://localhost:3000/">
+                        <img src="/images/backhome2.png" alt="move home"></img>
+                    </a>
+                </div>
+
+                <div className="sns-place">
+                    <img src="/images/SNS.png" alt="share SNS"/>
+                </div>
+                            
+                <div className="container-kids-home">
+                {/* 로고, 노래검색 */}
+                <header>
+                    <div className="kids_logo">
+                        <img src="/images/yourics_kids_logo.png" alt="Yourics kids"/>
+                    </div>
+                    <div className="kids_search">
+                        노래 검색 div
+                    </div>
+                </header>
+
+               
+
+                <div className="title-ul">
+                    <ul className="title-list">
+                        <Link to="/pinkpong">
+                        <li><img src="/images/pinkpong.png" alt="pinkpong" /></li>
+                        </Link>
+
+                        <li><img src="/images/tayo.png" alt="tayo"/></li>
+                        <li><img src="/images/bororo.png" alt="Bororo" /></li>
+                    </ul>
+                    <ul className="title-list">
+                        <li><img src="/images/zuzu.png" alt="secret juju"/></li>
+                        <li><img src="/images/frozen.png" alt="frozen"/></li>
+                        <li><img src="/images/congson.png" alt="congson"/></li>
+                    </ul>
+                </div>
+
+                </div>
             </div>
         ) 
     }
--- client/src/components/MainPage/MainPage.js
+++ client/src/components/MainPage/MainPage.js
@@ -64,53 +64,64 @@ class MainPage extends React.Component {
         const { clickAvoid, searchValue, videoId } = this.state;
 
         return (
-            <div className="container-main">
-                {/* 로고, 노래검색 */}
-                <header>
-                    <div className="logo">
-                        로고 이미지 div
-                        {clickAvoid && <span> Searching</span>}
-                    </div>
-                    <div className="search">
-                        <form onSubmit={this.searchSubmit}>
-                            <input type="text" value={searchValue} onChange={this.searchChange} />
-                            {
-                                !clickAvoid ?
-                                <input type="submit" value="검색" />
-                                :
-                                <input type="submit" disabled value="검색" />
-                            }
-                            
-                        </form>
-                    </div>
-                </header>
-
-                {/* 동영상, 가사 */}
-                <div className="main-div">
-                    <div className="video">
-                        {
-                            videoId && <iframe title="song" width="800" height="500" 
-                            src={`https://www.youtube.com/embed/${videoId}?autoplay=1`}
-                            frameBorder="0" 
-                            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen> 
-                            </iframe>
-                        }
-                        
-                    </div>
-                    <div className="lyrics">
-                        가사 div
-                    </div>
+            <div className="cotainer-main0">
+                <div className="backhome">
+                    <a href="http://localhost:3000/">
+                        <img src="/images/backhome2.png" alt="move home"></img>
+                    </a>
                 </div>
 
-                {/* SNS 공유 */}
-                <footer>
-                    <ul className="sns-list">
-                        <li>kakao</li>
-                        <li>insta</li>
-                        <li>fackbook</li>
-                    </ul>
-                </footer>
-            </div>
+                <div className="container-main">
+                    {/* 로고, 노래검색 */}
+                    <header>
+                        <div className="logo">
+                            
+                        <img src="/images/main_logo.png" alt="Yourics" /> 
+                        </div>
+
+                        <div className="search">
+                            <form onSubmit={this.searchSubmit}>
+                                <input type="text" value={searchValue} onChange={this.searchChange} />
+                                {
+                                    !clickAvoid ?
+                                    <input type="submit" value="검색" />
+                                    :
+                                    <input type="submit" disabled value="검색" />
+                                }
+                                
+                            </form>
+                        </div>
+                    
+                    </header>
+
+                    {/* 동영상, 가사 */}
+                    <div className="main-div">
+                        <div className="video">
+                            {
+                                videoId && <iframe title="song" width="800" height="500" 
+                                src={`https://www.youtube.com/embed/${videoId}?autoplay=1`}
+                                frameBorder="0" 
+                                allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen> 
+                                </iframe>
+                            }
+                            
+                        </div>
+                        <div className="lyrics">
+                            가사 div
+                        </div>
+                    </div>
+
+                    {/* SNS 공유 */}
+                    <footer>
+                        <ul className="sns-list">
+                            <li><img src="/images/sns_insta.png" alt="insta" /></li>
+                            <li><img src="/images/sns_fb.png" alt="facebook" /></li>
+                            <li><img src="/images/sns_kakao.png" alt="kakaotalk" /></li>
+                            <li><img src="/images/sns_twitter.png" alt="twitter" /></li>
+                        </ul>
+                    </footer>
+                </div>
+            </div>    
         ) 
     }
 }
