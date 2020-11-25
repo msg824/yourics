30230d37214cbd82b80aaee43f94394cd6474e82
msg824 <skfkgkdlaos@gmail.com>
2020-11-25 23:33:30 +0900

    불필요한 부분 제거

--- client/src/setupProxy.js
+++
@@ -1,9 +0,0 @@
-const proxy = require('http-proxy-middleware');
-
-let configs = {};
-process.env.NODE_ENV === 'development' ? configs = 'http://localhost:5000' : configs = '배포 IP주소';
-
-
-module.exports = function(app) {
-    app.use(proxy('/api/*', { target: 'http://localhost:5000' }));
-}
