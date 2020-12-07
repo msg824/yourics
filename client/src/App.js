ae1bbb00f69afb5586f5e8b2b324d72e5554a9c9
msg824 <skfkgkdlaos@gmail.com>
2020-12-08 02:17:48 +0900

    router 변경

--- client/src/App.js
+++ client/src/App.js
@@ -1,46 +1,42 @@
 import React from 'react';
 
-// CSS
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import MainPage from './components/MainPage/MainPage';
-import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-// React Router 정의
 function App() {
     return (
         <div className="App">
             <Router>
         	    <Switch>
-                    <Route exact path="/" children={<Home/>} /> {/* 아래 Home 함수 호출 */}
-                    <Route path="/main" children={<MainPage/>} /> {/* MainPage 호출 */}
+                    <Route exact path="/" children={<MainPage/>} />
                 </Switch>
             </Router>
         </div>
     );
 };
 
-// Home 페이지 html 여기에 구현 (App.css)
-function Home() {
-    return (
-        <div className="container-home">
-            <div className="home-background">
-                <div className="container-center">
-                    <div className="hometext">Watch the lyrics and Enjoy to the music.</div>
-                    <div>가사를 보며 음악을 감상해보세요.</div>
-                    <div className="yourics">YouRics</div>
-                    <div className="link-main">
-                        <Link to="/main">
-                            <div className="play">
-                                <img src="/images/onoff.png" alt="play"/>
-                            </div>
-                        </Link>
-                    </div>
-                </div>
-            </div>
-        </div>
-    )
-}
+// function Home() {
+//     return (
+//         <div className="container-home">
+//             <div className="home-background">
+//                 <div className="container-center">
+//                     <div className="hometext">Watch the lyrics and Enjoy to the music.</div>
+//                     <div>가사를 보며 음악을 감상해보세요.</div>
+//                     <div className="yourics">YouRics</div>
+//                     <div className="link-main">
+//                         <Link to="/main">
+//                             <div className="play">
+//                                 <img src="/images/onoff.png" alt="play"/>
+//                             </div>
+//                         </Link>
+//                     </div>
+//                 </div>
+//             </div>
+//         </div>
+//     )
+// }
 
 export default App;
